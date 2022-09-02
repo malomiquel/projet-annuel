db1c1ea61d4b521d2bec328c176643cac743e31c
TheoOui <theo.azouz@maincare.fr>
2022-09-02 10:28:51 +0200

    fixup et ajout V1 script

---
+++ lancement.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+fonctiondelancementfront(){
+	echo "Lancement du Front"
+	cd angular-front
+
+	}
+
+fonctiondelancementback
+	echo "Lancement du back"
+	cd back
+	}
+
+fonctiondelancementfront
+fonctiondelancementback
